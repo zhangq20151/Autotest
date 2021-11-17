ba4d91c9f1cac1e0d467fccf905639f71fec1f44
zhangq20151 <zhangq20151@163.com>
2021-11-17 17:16:28 +0800

    第10章 部分代码提交

---
+++ Chapter10/src/main/java/com/course/server/MyPostMethod.java
@@ -0,0 +1,69 @@
+package com.course.server;
+
+import com.course.bean.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@Api(value = "/", description = "这是我全部的post请求")
+@RequestMapping("/v1")
+public class MyPostMethod {
+
+    //这个变量是用来装我们cookies信息的
+    private static Cookie cookie;
+
+    //用户登录成功获取到cookies,然后再访问其他接口获取到列表
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    @ApiOperation(value = "登录接口，成功后获取cookies信息", httpMethod = "POST")
+    public String login(HttpServletResponse response,
+                        @RequestParam(value = "userName", required = true) String userName,
+                        @RequestParam(value = "passWord", required = true) String passWord) {
+        if (userName.equals("zhangsan") && passWord.equals("123456")) {
+            cookie = new Cookie("login", "true");
+            response.addCookie(cookie);
+            return "恭喜你登录成功了！";
+        }
+        return "用户名或密码错误！";
+
+    }
+
+
+    /**
+     *
+     */
+
+    @RequestMapping(value = "/getUserList", method = RequestMethod.POST)
+    @ApiOperation(value = "获取用户列表", httpMethod = "POST")
+    public String getUsetList(HttpServletRequest request,
+                            @RequestBody User u) {
+
+        User user;
+
+        //获取cookies
+        Cookie[] cookies = request.getCookies();
+        //验证cookies是否合法
+        for (Cookie c : cookies) {
+            if (c.getName() == "login"
+                    && c.getValue() == "true"
+                    && u.getUserName() == "zhangsan"
+                    && u.getPassWord() == "123456") {
+
+                user = new User();
+                user.setName("lisi");
+                user.setAge("18");
+                user.setSex("man");
+                return user.toString();
+
+            }
+        }
+
+        return "参数不合法";
+    }
+
+
+}
