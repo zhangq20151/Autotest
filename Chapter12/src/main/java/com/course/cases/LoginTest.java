08df57ad08f62d31798959eb8423c0ae5558aa83
zhangq_20151 <zhangq_20151@163.com>
2021-11-26 00:02:51 +0800

    第12章 部分代码上传

---
+++ Chapter12/src/main/java/com/course/cases/LoginTest.java
@@ -0,0 +1,61 @@
+package com.course.cases;
+
+import com.course.config.TestConfig;
+import com.course.model.InterfaceName;
+import com.course.model.LoginCase;
+import com.course.utils.ConfigFile;
+import com.course.utils.DatabaseUtil;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.ibatis.session.SqlSession;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+/**
+ * 从数据库中取出测试数据
+ * 取出测试地址
+ */
+public class LoginTest {
+
+    @BeforeTest(groups = "loginTrue",description = "测试准备工作，获取httpClient")
+    public void beforeTest(){
+        TestConfig.getUserInfoUrl = ConfigFile.getUrl(InterfaceName.GETUSERINFO);
+        TestConfig.getUserListUrl = ConfigFile.getUrl(InterfaceName.GETUSERLIST);
+        TestConfig.addUserUrl = ConfigFile.getUrl(InterfaceName.ADDUSER);
+        TestConfig.loginUrl = ConfigFile.getUrl(InterfaceName.LOGIN);
+        TestConfig.updateUserInfoUrl = ConfigFile.getUrl(InterfaceName.UPDATEUSERINFO);
+
+        TestConfig.defaultHttpClient = new DefaultHttpClient();
+    }
+
+    @Test(groups = "loginTrue",description = "用户登录成功接口测试")
+    public void loginTrue() throws IOException {
+        SqlSession session = DatabaseUtil.getSqlSession();
+        LoginCase loginCase = session.selectOne("loginCase",1);
+        System.out.println(loginCase.toString());
+        System.out.println(TestConfig.loginUrl);
+    }
+
+    @Test(groups = "loginFalse",description = "用户登录失败接口测试")
+    public void loginFalse() throws IOException {
+        SqlSession session = DatabaseUtil.getSqlSession();
+        LoginCase loginCase = session.selectOne("loginCase",2);
+        System.out.println(loginCase.toString());
+        System.out.println(TestConfig.loginUrl);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
