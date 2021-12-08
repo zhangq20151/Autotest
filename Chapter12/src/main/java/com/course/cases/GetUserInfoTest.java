44ad440b4bed37b78359343d17654e17f047caf2
zhangq_20151 <zhangq_20151@163.com>
2021-12-09 00:01:09 +0800

    第13章 部分代码上传

--- Chapter12/src/main/java/com/course/cases/GetUserInfoTest.java
+++ Chapter12/src/main/java/com/course/cases/GetUserInfoTest.java
@@ -2,11 +2,22 @@ package com.course.cases;
 
 import com.course.config.TestConfig;
 import com.course.model.GetUserInfoCase;
+import com.course.model.User;
 import com.course.utils.DatabaseUtil;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.util.EntityUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class GetUserInfoTest {
 
@@ -16,9 +27,41 @@ public class GetUserInfoTest {
         GetUserInfoCase getUserInfoCase = session.selectOne("getUserInfoCase",1);
         System.out.println(getUserInfoCase.toString());
         System.out.println(TestConfig.getUserInfoUrl);
+
+        JSONArray resultJson = getJsonResult(getUserInfoCase);
+
+        User user = session.selectOne(getUserInfoCase.getExpected(),getUserInfoCase);
+
+        List userList = new ArrayList();
+        userList.add(user);
+        JSONArray jsonArray = new JSONArray(userList);
+
+        Assert.assertEquals(jsonArray,resultJson);
+
+
     }
 
+    private JSONArray getJsonResult(GetUserInfoCase getUserInfoCase) throws IOException {
+        HttpPost post = new HttpPost(TestConfig.getUserListUrl);
+        JSONObject param = new JSONObject();
+        param.put("id",getUserInfoCase.getUserId());
 
+        post.setHeader("content-type","application/json");
 
+        StringEntity entity = new StringEntity(param.toString(),"utf-8");
+
+        post.setEntity(entity);
+
+        TestConfig.defaultHttpClient.setCookieStore(TestConfig.store);
+        String result;
+        HttpResponse response = TestConfig.defaultHttpClient.execute(post);
+
+        result = EntityUtils.toString(response.getEntity(),"utf-8");
+
+        List resultList = Arrays.asList(result);
+        JSONArray array = new JSONArray(resultList);
+
+        return  array;
+    }
 
 }
--- Chapter12/src/main/java/com/course/cases/UpdateUserInfoTest.java
+++ Chapter12/src/main/java/com/course/cases/UpdateUserInfoTest.java
@@ -2,8 +2,15 @@ package com.course.cases;
 
 import com.course.config.TestConfig;
 import com.course.model.UpdateUserInfoCase;
+import com.course.model.User;
 import com.course.utils.DatabaseUtil;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.util.EntityUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -16,7 +23,16 @@ public class UpdateUserInfoTest {
         UpdateUserInfoCase updateUserInfoCase = session.selectOne("updateUserInfoCase",1);
         System.out.println(updateUserInfoCase.toString());
         System.out.println(TestConfig.updateUserInfoUrl);
+        //发请求得结果
+        int result = getResult(updateUserInfoCase);
+        //验证
+        User user = session.selectOne(updateUserInfoCase.getExpected(),updateUserInfoCase);
+
+        Assert.assertNotNull(user);
+        Assert.assertNotNull(result);
     }
+
+
 
     @Test(dependsOnGroups = "loginTrue",description = "删除用户信息")
     public void deleteUser() throws IOException {
@@ -25,8 +41,40 @@ public class UpdateUserInfoTest {
         System.out.println(updateUserInfoCase.toString());
         System.out.println(TestConfig.updateUserInfoUrl);
 
+
+        //发请求得结果
+        int result = getResult(updateUserInfoCase);
+        //验证
+        User user = session.selectOne(updateUserInfoCase.getExpected(),updateUserInfoCase);
+
+        Assert.assertNotNull(user);
+        Assert.assertNotNull(result);
+
     }
 
+    private int getResult(UpdateUserInfoCase updateUserInfoCase) throws IOException {
+        HttpPost post = new HttpPost(TestConfig.updateUserInfoUrl);
+        JSONObject param = new JSONObject();
+        param.put("id",updateUserInfoCase.getId());
+        param.put("userName",updateUserInfoCase.getUserName());
+        param.put("sex",updateUserInfoCase.getSex());
+        param.put("age",updateUserInfoCase.getAge());
+        param.put("permission",updateUserInfoCase.getPermission());
+        param.put("isDelete",updateUserInfoCase.getIsDelete());
 
+        post.setHeader("content-type","application/json");
+        StringEntity entity = new StringEntity(param.toString(),"utf-8");
+        post.setEntity(entity);
+
+        TestConfig.defaultHttpClient.setCookieStore(TestConfig.store);
+
+        String result;
+        HttpResponse response = TestConfig.defaultHttpClient.execute(post);
+
+        result = EntityUtils.toString(response.getEntity(),"utf-8");
+
+        return Integer.parseInt(result);
+
+    }
 
 }
